1faa04726cfed898b904ae1df8b2d0001d1aadb9
Guilherme Beneti <guilhermebeneti@MacBook-Pro-de-Guilherme.local>
2019-03-05 21:42:42 +0000

    Upload project to git

---
+++ backend/src/api/todo/todo.js
@@ -0,0 +1,10 @@
+const retstul = require('node-restful')
+const mongoose = retstul.mongoose
+
+const todoSchema = new mongoose.Schema({
+    description: { type: String, required: true},
+    done: { type: Boolean, required: true, default: false},
+    createAt: { type: Date, default: Date.now}
+})
+
+module.exports = retstul.model('Todo', todoSchema)
